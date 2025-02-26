f1a7c3cf1eebb277b701c8e580dbcf94e8c496e3
fffilps <jmariwyatt@gmail.com>
2025-02-26 15:26:59 -0800

    refractored the app.js tenant and landlord code and initiated a header component along with component folder. reason is so i can start doing the layout for the react-router on each route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import './App.css'
 import { useState } from 'react'
+import TenantPage from './components/TenantPage';
+import LandlordPage from './components/LandlordPage';
 
 type Page = 'entry' | 'tenant' | 'landlord';
 
@@ -10,41 +12,7 @@ function App() {
 
   if (currentPage === 'tenant') {
     return (
-      <div className="bg-neutral-950 h-screen p-4">
-        <div className="flex h-3/5 rounded-lg overflow-hidden gap-4">
-          <div className="w-full rounded-lg overflow-hidden relative">
-            {/* Background Image */}
-            <img
-              src="/apartment_bg_img.jpg"
-              alt="Background"
-              className="absolute inset-0 object-cover w-full h-full"
-            />
-            {/* Dark Overlay */}
-            <div className="absolute inset-0 bg-neutral-900/75" />
-            {/* Hero Text */}
-            <div className="absolute inset-0 flex flex-col items-center justify-center">
-              <h1 className="text-4xl md:text-6xl font-medium text-orange-200 drop-shadow-lg text-center">
-                Welcome to <span className='text-orange-600 font-bold'>Smart Living</span>
-              </h1>
-              <h2 className="mt-4 text-2xl font-bold text-orange-200 drop-shadow-md text-center">
-                Experience comfort, embrace innovation.
-              </h2>
-            </div>
-          </div>
-
-          <div className="bg-neutral-900 rounded-lg p-4 overflow-auto">
-            <p className="text-white mb-2">
-              Notification 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
-            <p className="text-white mb-2">
-              Notification 2: Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-            <p className="text-white">
-              Notification 3: Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </p>
-          </div>
-        </div>
-      </div>
+      <TenantPage />
     );
   }
 
@@ -52,9 +20,7 @@ function App() {
 
   if (currentPage === 'landlord') {
     return (
-      <div className="flex justify-center items-center h-screen">
-        <h1 className="text-3xl font-bold">Landlord Page</h1>
-      </div>
+      <LandlordPage />
     );
   }
 
---
+++ frontend/src/components/LandlordPage.tsx
@@ -0,0 +1,11 @@
+
+
+const LandlordPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+        <h1 className="text-3xl font-bold">Landlord Page</h1>
+      </div>
+    );
+}
+
+export default LandlordPage

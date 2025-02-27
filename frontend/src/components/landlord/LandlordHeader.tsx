0543a2db1ca331b9e6171a8cb70d9fd976e37c76
fffilps <jmariwyatt@gmail.com>
2025-02-26 20:14:23 -0800

    theoretic header change when navigating to the header page/route.

---
+++ frontend/src/components/landlord/LandlordHeader.tsx
@@ -0,0 +1,11 @@
+
+
+const LandlordHeader = () => {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline">Landlord Page</h1>
+    </div>
+  )
+}
+
+export default LandlordHeader

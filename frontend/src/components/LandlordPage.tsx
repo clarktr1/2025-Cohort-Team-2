3e236273c6297da7be4d58dab64a8cff3e0bd641
fffilps <jmariwyatt@gmail.com>
2025-02-26 20:18:40 -0800

    update: dashboard page that loads when landlord.

--- frontend/src/components/LandlordPage.tsx
+++ frontend/src/components/LandlordPage.tsx
@@ -1,9 +1,17 @@
+import { Link, Route, Routes } from "react-router";
+import LandlordHeader from "./landlord/LandlordHeader";
 
 
 const LandlordPage = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-        <h1 className="text-3xl font-bold">Landlord Page</h1>
+        <nav>
+            <Link to={'/landlord'}>Dashboard</Link>
+        </nav>
+
+        <Routes>
+            <Route index element={<LandlordHeader />} />
+        </Routes>
       </div>
     );
 }

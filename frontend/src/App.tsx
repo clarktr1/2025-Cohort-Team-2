a78748bfe0bf1164ab8f26ba953e0169d89e1d14
Sabina <109144680+Srasulova@users.noreply.github.com>
2025-03-02 23:16:37 -0500

    fix the routing bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,20 @@
 import "./App.css";
-import TenantPage from "./components/TenantPage";
-import LandlordPage from "./components/LandlordPage";
-import { Route, Routes } from "react-router";
+import TenantPage from "./pages/Tenant/Tenant";
+import LandlordPage from "./pages/Landlord/Landlord";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import HomePage from "./pages/Home";
-import { RoleProvider } from './context/RoleContext';
-
+import HomePage from "./pages/Home/Home";
+import { RoleProvider } from "./context/RoleProvider";
 
 function App() {
-
-  // Entry Page with Background Image, Dark Overlay, and Buttons
   return (
     <RoleProvider>
       <Routes>
+        {/* Layout is the parent route */}
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path="/landlord" element={<LandlordPage />} />
-          <Route path="/tenant" element={<TenantPage />} />
+          <Route path="landlord" element={<LandlordPage />} />
+          <Route path="tenant" element={<TenantPage />} />
         </Route>
       </Routes>
     </RoleProvider>

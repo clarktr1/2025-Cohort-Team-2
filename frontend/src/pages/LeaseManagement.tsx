608dec316e252ca0c3dd956b7fbab9017946e26e
lattiric <78038846+lattiric@users.noreply.github.com>
2025-03-03 18:54:53 -0600

    Finished visual layout of page lease display window disapears and reappears added lease info display and selection within list

--- frontend/src/pages/LeaseManagement.tsx
+++ frontend/src/pages/LeaseManagement.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useState, useEffect } from "react";
 // import {FaFire} from 'react-icons/fa';
 
 const LeaseManagement: React.FC = () => {
+
+    //display functions
+    const leaseClosed = () => {
+        const leaseDisplay = document.getElementById("lease-display");
+        if(leaseDisplay) {
+            leaseDisplay.className = "bg-white rounded-3xl h-9/10 w-3/5 p-4 mt-20 mr-5 overflow-auto scale-0"
+        }
+    };
+
     return (
         <>
             {/* Background ----- fix it overflowing onto the sidebare */}
@@ -9,10 +19,15 @@ const LeaseManagement: React.FC = () => {
                              bg-neutral-700 
                              felx-grow h-screen ml-22">
                 
-                {/* Titles */}
-                <div className="absolute top-0 left-0 right-0 bg-neutral-700  h-22 w-screen">
-                    <h1 className="top-0 left-0 right-0 text-white text-4xl ml-22 p-4">
+                {/* Title */}
+                <div className="absolute top-0 left-0 right-0 bg-neutral-700  
+                                h-22 w-screen
+                                flex gap-0">
+                    <h1 className="w-1/2 text-white text-4xl ml-22 p-4">
                         My Leases:
+                    </h1>
+                    <h1 id="test-text" className="w-1/2 bg-neutral-700   text-white text-4xl p-4">
+                        Testing Text:
                     </h1>
                 </div>
                 
@@ -20,12 +35,17 @@ const LeaseManagement: React.FC = () => {
                 {/* Display */}
                 <div className="bg-neutral-900 rounded-2xl h-9/10 flex-grow mt-20 overflow-auto">
                     {/* Header */}
-                    <LeaseItem name="lease1" />
-                    <LeaseItem name="lease2" />
-                    <LeaseItem name="lease3" />
-                    <LeaseItem name="lease4" />
+                    <LeaseItem name="lease1 this is testing bc this might be long" status={0} date="02/19"/>
+                    <LeaseItem name="lease2" status={0} date="10/02"/>
+                    <LeaseItem name="lease3" status={1} date="10/02"/>
+                    <LeaseItem name="lease4" status={1} date="10/02"/>
                 </div>
-                <div className="bg-blue-200 rounded-3xl h-9/10 w-3/5 p-4 mt-20 mr-5 overflow-auto scale-0">
+                {/* Lease Display */}
+                <div id="lease-display" className="bg-white rounded-3xl h-9/10 w-3/5 p-4 mt-20 mr-5 overflow-auto scale-0">
+                    <button onClick={leaseClosed} className="absolute top-2 right-4 text-black scale-150
+                                     hover:text-orange-400 rounded-3xl hover:rounded-xl transition-all duration-300 ">
+                        X
+                    </button>
                 </div>
             </div>
 
@@ -51,22 +71,70 @@ const LeaseManagement: React.FC = () => {
 
 
 //sidebar class and associated tooltips
-const SideBarIcon = ({ icon, text = 'tooltip' }: {icon: string, text: string}) => (
-    <div className="sidebar-name group">
-        {icon}
+const SideBarIcon = ({ icon, text = 'tooltip' }: {icon: string, text: string}) => {
+    
+    //logic for sidebar button click
+    const [sidebarStatus, setSidebarStatus] = useState("All");
 
-        <span className="sidebar-tooltip group-hover:scale-100">
-            {text}
-        </span>
-    </div>
-);
+    const sbButtonClicked = () => {
+        const testText = document.getElementById("test-text");
+        if(testText) {
+            setSidebarStatus(icon);
+            testText.innerHTML = sidebarStatus;
+        };
+    };
+
+    useEffect(() => {
+        const leaseDisplay = document.getElementById("lease-display");
+        if (leaseDisplay) {
+            const childElements = leaseDisplay.querySelectorAll("div"); // Select all divs inside
+            console.log("reached inside if")
+            childElements.forEach((child) => {
+                child.className = "scale-0"
+            });
+        }
+    }, [sidebarStatus]);
+
+    return (
+        <div className="sidebar-name group">
+            
+            <button onClick={sbButtonClicked}> {icon} </button>
+
+            <span className="sidebar-tooltip group-hover:scale-100">
+                {text}
+            </span>
+        </div>
+    );
+};
 
 
-//sidebar class and associated tooltips
-const LeaseItem = ({ name}: {name: string}) => (
-    <div className="lease-item">
-        {name}
-    </div>
-);
+//lease class and display opening
+const LeaseItem = ({name, status, date}: {name: string, status: number, date: string}) => {
+    
+     //lease button click
+    const leaseButtonClicked = () => {
+        const testText = document.getElementById("test-text");
+        const leaseDisplay = document.getElementById("lease-display")
+
+        if(testText) {
+            testText.innerHTML = "Lease Clicked: "+name;
+        }
+        if(leaseDisplay) {
+            leaseDisplay.className = "bg-white rounded-3xl h-9/10 w-3/5 p-4 mt-20 mr-5 overflow-auto scale-100"
+        }
+    };
+
+    return (
+        <div className="lease-item">
+            <button onClick={leaseButtonClicked} className="lease-button">
+                <div className="w-35 ml-3 truncate">{name}</div>
+                <div id="statusText" className="w-35 ml-3 truncate">
+                    {status == 1 ? "Completed" : "Signing Required"}
+                </div>
+                <div>{date}</div>
+            </button>
+        </div>
+    );
+};
 
 export default LeaseManagement;

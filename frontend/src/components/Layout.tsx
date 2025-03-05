c5d4e37672a5e31748db1c72cee29ed1f5bc7912
Sabina <109144680+Srasulova@users.noreply.github.com>
2025-03-04 21:31:01 -0500

    make reusable navlistelement and apply to tenant navbar

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
+import NavBar from "./Navbar";
+import { useLocation } from "react-router-dom";
 
 const Layout = () => {
+  const location = useLocation();
+  const showHeader = location.pathname.startsWith("/landlord") || location.pathname.startsWith("/tenant");
+
   return (
     <div>
-      <Header />
+      {showHeader && <NavBar />}
       {/* This is where nested routes/components will be rendered */}
       <Outlet />
     </div>
---
+++ frontend/src/data/tenantNav.ts
@@ -0,0 +1,11 @@
+// tenantNav.ts
+export const tenantNav = [
+    { to: "/", navLinkName: "Home" },
+    { to: "/lease", navLinkName: "Lease Management" },
+    { to: "/payments", navLinkName: "Payments" },
+    { to: "/access", navLinkName: "Access Controls" },
+    { to: "/maintenance", navLinkName: "Maintenance" },
+    { to: "/deliveries", navLinkName: "Deliveries" },
+    { to: "/complaints", navLinkName: "Complaints" },
+  ];
+  

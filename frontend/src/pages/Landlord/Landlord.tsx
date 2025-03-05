57ed46fa7765d7ae9a3f0affc82b913b5aae76a6
Sabina <109144680+Srasulova@users.noreply.github.com>
2025-03-04 21:58:04 -0500

    add dummy element to Landlord page

--- frontend/src/pages/Landlord/Landlord.tsx
+++ frontend/src/pages/Landlord/Landlord.tsx
@@ -1,17 +1,17 @@
-import { Link, Route, Routes } from "react-router";
-import LandlordHeader from "../../components/landlord/LandlordHeader";
 
 
 const LandlordPage = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <nav>
-        <Link to={'/landlord'}>Dashboard</Link>
-      </nav>
+    <div className="flex justify-center items-center bg-neutral-950 ">
+      <div className="bg-neutral-900 m-2 rounded-lg w-full">
+        <h1 className="text-4xl font-bold text-orange-200 text-center">
+          Landlord Page
+        </h1>
+        <p className="text-white mt-4">
+          This is the Landlord Page. Here, the landlord can view all the properties they own, manage the properties, and view the tenants living in their properties.
+        </p>
 
-      <Routes>
-        <Route index element={<LandlordHeader />} />
-      </Routes>
+      </div>
     </div>
   );
 }

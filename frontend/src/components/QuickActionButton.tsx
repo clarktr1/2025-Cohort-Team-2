22bad7e4b31650146818f63c5f6eed27c4650bfc
Sabina <109144680+Srasulova@users.noreply.github.com>
2025-03-04 22:58:34 -0500

    create quick action button

---
+++ frontend/src/components/QuickActionButton.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface QuickActionButtonProps {
+    text: string;
+    onClick?: () => void;
+}
+
+const QuickActionButton: React.FC<QuickActionButtonProps> = ({ text, onClick }) => {
+    return (
+        <div className="w-40 h-40 p-1 bg-gradient-to-r from-orange-500 to-orange-100 rounded-full">
+            <button
+                onClick={onClick}
+                className="w-full h-full rounded-full flex items-center justify-center bg-neutral-950 text-orange-100 font-bold hover:bg-orange-100 hover:text-neutral-900 transition-colors duration-300 ease-in-out focus:outline-none"
+            >
+                {text}
+            </button>
+        </div>
+    );
+};
+
+export default QuickActionButton;

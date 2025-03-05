03d7ecddf6d87da7b4ef8010afb2786d7e0ffede
Sabina <109144680+Srasulova@users.noreply.github.com>
2025-03-04 21:31:39 -0500

    add dummy styling

--- frontend/src/components/landlord/LandlordHeader.tsx
+++ frontend/src/components/landlord/LandlordHeader.tsx
@@ -3,7 +3,7 @@
 const LandlordHeader = () => {
   return (
     <div>
-        <h1 className="text-3xl font-bold underline">Landlord Page</h1>
+      <h1 className="text-3xl text-red-500 font-bold underline">Landlord Page</h1>
     </div>
   )
 }

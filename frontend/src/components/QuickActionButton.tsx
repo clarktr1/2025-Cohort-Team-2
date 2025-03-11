d0620e5bbe2cd0cf105248a62030db647b559316
Sabina <109144680+Srasulova@users.noreply.github.com>
2025-03-10 22:01:51 -0400

    change divs to ul and li elements on quick action buttons

--- frontend/src/components/QuickActionButton.tsx
+++ frontend/src/components/QuickActionButton.tsx
@@ -7,14 +7,14 @@ interface QuickActionButtonProps {
 
 const QuickActionButton: React.FC<QuickActionButtonProps> = ({ text, onClick }) => {
     return (
-        <div className="w-40 h-40 p-1 bg-gradient-to-r from-orange-500 to-orange-100 rounded-full">
+        <li className="w-40 h-40 p-1 bg-gradient-to-r from-orange-500 to-orange-100 rounded-full">
             <button
                 onClick={onClick}
                 className="w-full h-full rounded-full flex items-center justify-center bg-neutral-950 text-orange-100 font-bold hover:bg-orange-100 hover:text-neutral-900 transition-colors duration-300 ease-in-out focus:outline-none"
             >
                 {text}
             </button>
-        </div>
+        </li>
     );
 };
 
--- frontend/src/components/QuickActions.tsx
+++ frontend/src/components/QuickActions.tsx
@@ -14,11 +14,11 @@ const QuickActions: React.FC<QuickActionsProps> = ({ actions }) => {
     return (
         <div className=" bg-neutral-900 rounded-b-lg py-10">
             <h2 className="font-bold text-orange-100 mb-10 tracking-widest text-center text-4xl">Quick Actions</h2>
-            <div className="flex gap-4 items-center justify-center">
+            <ul className="flex gap-4 items-center justify-center list-none">
                 {actions.map((action, index) => (
                     <QuickActionButton key={index} text={action.text} onClick={action.onClick} />
                 ))}
-            </div>
+            </ul>
         </div>
     );
 };

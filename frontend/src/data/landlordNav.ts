60696305919365caad3d35235e8141cf14e22d66
Sabina <109144680+Srasulova@users.noreply.github.com>
2025-03-10 22:08:39 -0400

    remove unnecessary navbar items

--- frontend/src/data/landlordNav.ts
+++ frontend/src/data/landlordNav.ts
@@ -1,8 +1,8 @@
 // landlordNav.ts
 export const landlordNav = [
     { to: "/", navLinkName: "Home" },
-    { to: "/tenant-management", navLinkName: "Tenant Management" },
-    { to: "/maintenance-requests", navLinkName: "Maintenance Requests" },
+    { to: "/tenant_management", navLinkName: "Tenant Management" },
+    { to: "/lease_agreements", navLinkName: "Lease Agreements" },
     { to: "/complaints", navLinkName: "Complaints" },
     { to: "/communication", navLinkName: "Communication & Announcements" },
   ];
--- frontend/src/data/tenantNav.ts
+++ frontend/src/data/tenantNav.ts
@@ -2,9 +2,7 @@
 export const tenantNav = [
     { to: "/", navLinkName: "Home" },
     { to: "/lease", navLinkName: "Lease Management" },
-    { to: "/payments", navLinkName: "Payments" },
     { to: "/access", navLinkName: "Access Controls" },
-    { to: "/maintenance", navLinkName: "Maintenance" },
     { to: "/deliveries", navLinkName: "Deliveries" },
     { to: "/complaints", navLinkName: "Complaints" },
   ];

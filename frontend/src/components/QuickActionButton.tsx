8d9aa06d3785b01956c36e1e100e4b1bb80b3d01
Sabina <109144680+Srasulova@users.noreply.github.com>
2025-03-12 00:29:35 -0400

    add cursor pointer on hover

--- frontend/src/components/QuickActionButton.tsx
+++ frontend/src/components/QuickActionButton.tsx
@@ -10,7 +10,7 @@ const QuickActionButton: React.FC<QuickActionButtonProps> = ({ text, onClick }) 
         <li className="w-40 h-40 p-1 bg-gradient-to-r from-orange-500 to-orange-100 rounded-full">
             <button
                 onClick={onClick}
-                className="w-full p-4 h-full rounded-full flex items-center justify-center bg-neutral-950 text-orange-100 font-bold hover:bg-orange-100 hover:text-neutral-900 transition-colors duration-300 ease-in-out focus:outline-none"
+                className="w-full p-4 h-full rounded-full flex items-center justify-center bg-neutral-950 text-orange-100 font-bold hover:bg-orange-100 hover:text-neutral-900 transition-colors duration-300 ease-in-out focus:outline-none hover:cursor-pointer"
             >
                 {text}
             </button>

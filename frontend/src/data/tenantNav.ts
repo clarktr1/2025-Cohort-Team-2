95beaac8c6b4cf9ea663ff9f6a2fc29d436d4fd6
Sabina <109144680+Srasulova@users.noreply.github.com>
2025-03-17 07:01:52 -0400

    add entry page and fix the "home" navigation

--- frontend/src/data/tenantNav.ts
+++ frontend/src/data/tenantNav.ts
@@ -1,5 +1,6 @@
 export const tenantNav = [
-    { to: "/", navLinkName: "Home" },
+  { to: "/", navLinkName: "Entry Page" },
+    { to: "/tenant", navLinkName: "Home" },
     { to: "/lease", navLinkName: "Lease Management" },
     { to: "/access", navLinkName: "Access Controls" },
     { to: "/deliveries", navLinkName: "Deliveries" },
